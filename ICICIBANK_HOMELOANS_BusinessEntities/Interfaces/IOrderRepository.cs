e32a156c00905ede14e89fa0882a967b2c1e96bd
venkat <dell@DESKTOP-S8CP3CH>
2026-05-31 12:30:19 +0530

    userstory-1234581-CreateCrudOperationsAPIsForOrders

---
+++ ICICIBANK_HOMELOANS_BusinessEntities/Interfaces/IOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using ICICIBANK_HOMELOANS_BusinessEntities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICICIBANK_HOMELOANS_BusinessEntities.Interfaces
+{
+    public interface IOrderRepository
+    {
+        public Task<List<Orders>> GetOrders();
+        public Task<Orders> GetOrderById(int orderid);
+         public Task<int> AddOrder(Orders order);
+         public Task<int> UpdateOrder(Orders order);
+         public Task<bool> DeleteOrder(int orderid);
+    }
+}
---
+++ ICICIBANK_HOMELOANS_BusinessEntities/Models/Orders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICICIBANK_HOMELOANS_BusinessEntities.Models
+{
+    public class Orders
+    {
+        public int orderid { get; set; }
+        public string ordername { get; set; }
+        public string orderlocation { get; set; }
+    }
+}
